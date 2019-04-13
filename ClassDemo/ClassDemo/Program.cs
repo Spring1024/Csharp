03ba4a08df61031627c23cb1f4516517bbb238a0
spring1024 <x1819669614@163.com>
2019-04-13 11:14:42 +0800

    week06

---
+++ ClassDemo/ClassDemo/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    class Program
+    {
+        public class Student
+        {
+            private string _Name;
+            private string _Sex;
+            private int _Age;
+            public string Name
+            {
+                get { return _Name; }
+                set { _Name = value; }
+            }
+            public string Sex
+            {
+                get { return _Sex; }
+                set 
+                {
+                    if (value != "男" || value != "nv")
+                        _Sex = "男";
+                    else
+                        _Sex = value;
+                }
+            }
+            public int Age
+            {
+                get { return _Age; }
+                set
+                {
+                    if (value >= 0 && value <= 100)
+                        _Age = value;
+                    else
+                        _Age = 18;
+                }
+            }
+            public Student() { }
+            public Student(string name)
+            {
+                Name=name;
+                Sex = "男";
+                Age = 0;
+            }
+            public Student(string name, string sex)
+            {
+                Name = name;
+                Sex = sex;
+                Age = 0;
+            }
+            public Student(string name, string sex, int age)
+            {
+                Name = name;
+                Sex = sex;
+                Age = age;
+            }
+            public void Show()
+            {
+                Console.WriteLine("姓名：{0}   性别：{1}   年龄：{2}", Name, Sex, Age);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Student stu1 = new Student("cherry", "女", 21);
+            Student stu = new Student();
+            stu.Name = "spring";
+            stu.Sex = "男";
+            stu.Age = 19;
+            stu.Show();
+            stu1.Show();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StructDemo/StructDemo/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructDemo
+{
+    class Program
+    {
+        public struct Student
+        {
+            private string _Name;
+            private string _Sex;
+            private int _Age;
+            public string Name
+            {
+                get { return _Name;}
+                set { _Name = value; }
+            }
+            public string Sex
+            {
+                get { return _Sex; }
+                set
+                {
+                    if (value != "男" || value != "女")
+                        _Sex = "男";
+                    else
+                        _Sex = value;
+                }
+            }
+            public int Age
+            {
+                get { return _Age; }
+                set
+                {
+                    if (value >= 0 && value <= 100)
+                        _Age = value;
+                    else
+                        _Age = 18;
+                }
+            }
+            public Student(string name, string sex, int age)
+            {
+                _Name = name;
+                _Sex = sex;
+                _Age = age;
+            }
+            public void Show()
+            {
+                Console.WriteLine("姓名:{0} 性别:{1} 年龄:{2}", Name, Sex, Age);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Student stu=new Student("spring","男",19);
+            stu.Show();
+            Console.ReadKey();
+
+            Student stu1;
+            stu1.Name = "spring";
+
+        }
+    }
+}
